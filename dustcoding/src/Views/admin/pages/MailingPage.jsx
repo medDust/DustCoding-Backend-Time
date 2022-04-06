f4f09d3fb6748b39adf44feb8dba1b30e589436f
mohameddida <mohameddida891@gmail.com>
2022-04-06 10:58:48 +0100

    push main

--- dustcoding/src/Views/admin/pages/MailingPage.jsx
+++ dustcoding/src/Views/admin/pages/MailingPage.jsx
@@ -11,10 +11,8 @@ const MailingPage = () => {
     GetMails()
       .then((response) => {
         const mails = response.data;
-
         setMails(mails);
       })
-
       .catch((err) => console.log(err));
   }, []);
   const MailsList = Mails.map((mail) => (
@@ -43,7 +41,7 @@ const MailingPage = () => {
       <div className="ml-14 mt-14 mb-10 grid h-full md:ml-64 ">
         <div className="my-5 flex items-center justify-between p-2 ">
           <span className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            List Employer
+            List Mailing
           </span>
         </div>
 
@@ -54,9 +52,9 @@ const MailingPage = () => {
                 <tr className="border-b bg-gray-50 text-left text-xs font-semibold uppercase tracking-wide text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400">
                   <th className="px-4 py-3">Client</th>
 
-                  <th className="px-4 py-3">Status</th>
-                  <th className="px-4 py-3">type</th>
-                  <th className="px-4 py-3">Date</th>
+                  <th className="px-4 py-3">E-Mail</th>
+                  <th className="px-4 py-3">Phone</th>
+                  <th className="px-4 py-3">Details</th>
                 </tr>
               </thead>
               <tbody className="divide-y bg-white dark:divide-gray-700 dark:bg-gray-800">
